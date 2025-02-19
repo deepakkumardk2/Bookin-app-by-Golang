553a3edfc144b36125ed8c16cb630329184af7f5
Deepak Kumar <dk79944@gmail.com>
2025-02-19 17:02:15 +0530

    this is my 1st commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"	
+func main() {
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get your ticket here to attend the conference")
+
+	name := "Go Conference"
+	fmt.Println("Conference Name: ", name) 
+	fmt.Println("Conference Name: ", name) 
+	
+	
+}
+

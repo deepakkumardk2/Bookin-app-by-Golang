ee9275e98e1165291b770df9e85d962a8a56313c
Deepak Kumar <dk79944@gmail.com>
2025-02-19 17:31:01 +0530

    4th changes

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ func main() {
 	fmt.Println("Welcome to our conference booking application")
 	fmt.Println("Get your ticket here to attend the conference")
 
+	conferenceName :="Go Conference"
+	fmt.Println("Conference Name: ", conferenceName)
+
 	name := "Go Conference"
 	fmt.Println("Conference Name: ", name) 
 	//golang is a very powerful language
